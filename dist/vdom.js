c1a683afb8ac7cbc39edde932f2c354c46b165ff
Matthew Keas <matthiasak@gmail.com>
2015-12-03 12:31:03 -0600

    new build

--- dist/vdom.js
+++ dist/vdom.js
@@ -5,6 +5,9 @@ var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = 
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 /*
     todo:
     - diff algo
@@ -27,6 +30,27 @@ VDOM structure:
 }
  */
 
+var pool = function pool() {
+    var pool = [];
+
+    var get = function get() {
+        return pool.length ? pool.shift() : {};
+    };
+
+    var recycle = function recycle(obj) {
+        Object.keys(obj).forEach(function (k) {
+            return delete obj[k];
+        });
+        pool.push(obj);
+    };
+
+    return { get: get, recycle: recycle };
+};
+
+var POOL = pool();
+
+var simpleRenderingMode = false;
+
 var class_id_regex = function class_id_regex() {
     return (/[#\.][^#\.]+/ig
     );
@@ -40,7 +64,12 @@ var parseSelector = function parseSelector(s) {
     var test = null,
         tag = tagName_regex().exec(s).slice(1)[0],
         reg = class_id_regex(),
-        vdom = { tag: tag || '', classes: [], ids: [] };
+        vdom = POOL.get();
+
+    vdom.tag = tag || '';
+    vdom.classes = [];
+    vdom.ids = [];
+
     if (tag) s = s.substr(tag.length);
     while ((test = reg.exec(s)) !== null) {
         test = test[0];
@@ -54,17 +83,44 @@ var m = exports.m = function m(selector) {
         children[_key - 2] = arguments[_key];
     }
 
-    var attrs = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+    var attrs = arguments.length <= 1 || arguments[1] === undefined ? POOL.get() : arguments[1];
 
-    if (attrs.hasOwnProperty('tag') || typeof attrs === "string") {
-        children.unshift(attrs);
-        attrs = {};
+    if (attrs.tag || !(attrs instanceof Object) || attrs instanceof Array) {
+        if (attrs instanceof Array) children.unshift.apply(children, _toConsumableArray(attrs));else children.unshift(attrs);
+        attrs = POOL.get();
     }
-
     var vdom = parseSelector(selector);
     if (children.length) vdom.children = children;
     vdom.attrs = attrs;
     return vdom;
+};
+
+var reservedAttrs = ['className', 'id'];
+
+// creatign html, strip events from DOM element... for now just deleting
+var stripEvents = function stripEvents(_ref) {
+    var attrs = _ref.attrs;
+    return attrs ? Object.keys(attrs).filter(function (x) {
+        return (/^on[a-zA-Z]/.exec(x)
+        );
+    }).reduce(function (a, name) {
+        a[name] = attrs[name];
+        delete attrs[name];
+        return a;
+    }, POOL.get()) : POOL.get();
+};
+
+var applyEvents = function applyEvents(events, el) {
+    Object.keys(el).filter(function (x) {
+        return (/^on[a-zA-Z]/.exec(x)
+        );
+    }).forEach(function (x) {
+        return delete el[x];
+    });
+
+    Object.keys(events).forEach(function (name) {
+        return el.addEventListener(name.substr(2).toLowerCase(), events[name]);
+    });
 };
 
 var html = exports.html = function html(vdom) {
@@ -78,36 +134,62 @@ var html = exports.html = function html(vdom) {
     var classes = vdom.classes;
     var attrs = vdom.attrs;
     var children = vdom.children;
-    var id = ids.length ? 'ids="' + ids.join(' ') + '"' : '';
-    var _class = classes.length ? 'class="' + classes.join(' ') + '"' : '';
-
+    var id = 'id="' + (ids || []).concat(attrs ? attrs.id : '').join(' ') + '"';
+    var _class = 'class="' + (classes || []).concat(attrs ? attrs.className : '').join(' ') + '"';
     var closing = children ? children.map(function (c) {
         return html(c);
     }).join(' ') + '</' + tag + '>' : '';
+    // TODO: figure out wtf todo here?
+    // maybe just never use these, only use html() on server rendering?
+    var events = stripEvents(vdom);
+    var _attrs = Object.keys(attrs || POOL.get()).filter(function (x) {
+        return reservedAttrs.indexOf(x) === -1;
+    }).reduce(function (a, v, i, arr) {
+        return a + ' ' + v + '="' + attrs[v] + '"';
+    }, '');
 
-    return '<' + tag + ' ' + id + ' ' + _class + ' ' + (!closing ? '/' : '') + '>' + closing;
+    POOL.recycle(vdom);
+
+    return '<' + tag + ' ' + id + ' ' + _class + ' ' + _attrs + ' ' + (!children ? '/' : '') + '>' + closing;
 };
 
 var rAF = requestAnimationFrame || webkitRequestAnimationFrame || mozRequestAnimationFrame || function (cb) {
-    return setTimeout(cb, 0);
+    return setTimeout(cb, 16.6);
 };
 
 var mounts = new Map();
 
 var mount = exports.mount = function mount(fn, el) {
-    render(fn, el);
+    render(fn(), el);
     mounts.set(el, fn);
 };
 
-var _render = undefined;
-var render = exports.render = function render(fn, el) {
-    // let b = +new Date
-    _render = html(fn());
-    rAF(function () {
-        el.innerHTML = _render;
-    });
-    // log((+new Date - b)+'ms')
+var debounce = function debounce(func, wait) {
+    var timeout = null,
+        calls = 0,
+        memo = function memo(func) {
+        for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+            args[_key2 - 1] = arguments[_key2];
+        }
+
+        timeout = null;
+        func.apply(undefined, args);
+    };
+    return function () {
+        for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+            args[_key3] = arguments[_key3];
+        }
+
+        clearTimeout(timeout);
+        timeout = setTimeout(memo.bind.apply(memo, [null, func].concat(args)), wait);
+    };
 };
+
+var render = debounce(function (vdom, el) {
+    rAF(function () {
+        return simpleRenderingMode ? simpleApply(vdom, el) : applyUpdates(vdom, el);
+    });
+}, 16);
 
 var update = exports.update = function update() {
     var _iteratorNormalCompletion = true;
@@ -121,7 +203,7 @@ var update = exports.update = function update() {
             var el = _step$value[0];
             var fn = _step$value[1];
 
-            render(fn, el);
+            render(fn(), el);
         }
     } catch (err) {
         _didIteratorError = true;
@@ -139,8 +221,102 @@ var update = exports.update = function update() {
     }
 };
 
+var createTag = function createTag() {
+    var vdom = arguments.length <= 0 || arguments[0] === undefined ? POOL.get() : arguments[0];
+
+    if (!(vdom instanceof Object)) return document.createTextNode(vdom);
+    var tag = vdom.tag;
+    var attrs = vdom.attrs;
+    var ids = vdom.ids;
+    var classes = vdom.classes;
+    var x = document.createElement(tag);
+    var events = stripEvents(vdom);
+
+    applyEvents(events, x);
+    attrs && Object.keys(attrs).forEach(function (attr) {
+        return x.setAttribute(attr, attrs[attr]);
+    });
+    x.id = ids ? ids.join(' ') : '';
+    x.className = classes ? classes.join(' ') : '';
+    return x;
+};
+
+var simpleApply = function simpleApply(vdom, el) {
+    return el.innerHTML = html(vdom);
+};
+
+var applyUpdates = function applyUpdates(vdom, el) {
+    if (!vdom) {
+        el && el.parentElement && el.parentElement.removeChild(el);
+        return;
+    }
+
+    var __el = el && el.children,
+        __v = vdom && vdom.children;
+
+    if (vdom instanceof Array) {
+        __v = vdom;
+    } else if (vdom instanceof Object && vdom.tag) {
+        if (!el || el.tagName !== vdom.tag) {
+            var t = createTag(vdom);
+            // t.innerHTML = html(vdom.children)
+            el.parentElement.insertBefore(t, el);
+            el.parentElement.removeChild(el);
+            return applyUpdates(__v, t);
+        }
+    } else {
+        var t = document.createTextNode(vdom);
+        el.parentElement.insertBefore(t, el);
+        el.parentElement.removeChild(el);
+        return;
+    }
+
+    var len = Math.max(__el.length, __v.length);
+    for (var i = 0; i < len; i++) {
+        var v = __v[i],
+            d = __el[i];
+
+        if (v && d) {
+            applyUpdates(v, d);
+        } else if (v && !d) {
+            var t = createTag(v);
+            applyUpdates(v.children, t);
+            el.appendChild(t);
+        } else if (!vdom && d) {
+            d.parentElement.removeChild(d);
+        }
+    }
+
+    // currently clears/zeroes out the data prematurely, need to figure this out
+    // rAF(() => POOL.recycle(vdom))
+};
+
 var qs = exports.qs = function qs() {
     var s = arguments.length <= 0 || arguments[0] === undefined ? 'body' : arguments[0];
     var el = arguments.length <= 1 || arguments[1] === undefined ? document : arguments[1];
     return el.querySelector(s);
 };
+
+/*
+usage:
+
+let component = () =>
+    new Array(20).fill(true).map(x =>
+        m('div', {onMouseOver: e => log(e.target.innerHTML)}, range(1,100)))
+
+client-side
+-----
+mount(component, qs())
+
+server-side (Express)
+-----
+res.send(html(component()))
+
+client-side constant re-rendering
+-----
+const run = () => {
+    setTimeout(run, 20)
+    update()
+}
+run()
+*/
--- src/vdom.js
+++ src/vdom.js
@@ -20,8 +20,26 @@ VDOM structure:
 }
  */
 
-const
-    class_id_regex = () => {
+const pool = () => {
+    let pool = []
+
+    const get = () => {
+        return pool.length ? pool.shift() : {}
+    }
+
+    const recycle = (obj) => {
+        Object.keys(obj).forEach(k => delete obj[k])
+        pool.push(obj)
+    }
+
+    return {get, recycle}
+}
+
+const POOL = pool()
+
+const simpleRenderingMode = false
+
+const class_id_regex = () => {
         return /[#\.][^#\.]+/ig
     },
     tagName_regex = () => {
@@ -32,7 +50,12 @@ const parseSelector = s => {
     let test = null,
         tag = tagName_regex().exec(s).slice(1)[0],
         reg = class_id_regex(),
-        vdom = {tag: tag||'', classes:[], ids:[]}
+        vdom = POOL.get()
+
+    vdom.tag = tag || ''
+    vdom.classes = []
+    vdom.ids = []
+
     if(tag) s = s.substr(tag.length)
     while((test = reg.exec(s)) !== null){
         test = test[0]
@@ -44,12 +67,12 @@ const parseSelector = s => {
     return vdom
 }
 
-export const m = (selector, attrs={}, ...children) => {
-    if(attrs.hasOwnProperty('tag') || typeof attrs === "string"){
-        children.unshift(attrs)
-        attrs = {}
+export const m = (selector, attrs=POOL.get(), ...children) => {
+    if(attrs.tag || !(attrs instanceof Object) || attrs instanceof Array){
+        if(attrs instanceof Array) children.unshift(...attrs)
+        else children.unshift(attrs)
+        attrs = POOL.get()
     }
-
     let vdom = parseSelector(selector)
     if(children.length)
         vdom.children = children
@@ -57,46 +80,170 @@ export const m = (selector, attrs={}, ...children) => {
     return vdom
 }
 
+const reservedAttrs = ['className','id']
+
+// creatign html, strip events from DOM element... for now just deleting
+const stripEvents = ({attrs}) =>
+    attrs ?
+        Object.keys(attrs)
+            .filter(x => /^on[a-zA-Z]/.exec(x))
+            .reduce((a, name) => {
+                a[name] = attrs[name]
+                delete attrs[name]
+                return a
+            }, POOL.get()) :
+        POOL.get()
+
+const applyEvents = (events, el) => {
+    Object.keys(el)
+        .filter(x => /^on[a-zA-Z]/.exec(x))
+        .forEach(x => delete el[x])
+
+    Object.keys(events).forEach(name =>
+        el.addEventListener(name.substr(2).toLowerCase(), events[name]))
+}
+
 export const html = vdom => {
     if(vdom instanceof Array) return vdom.map(c => html(c)).join(' ')
     if(!(vdom instanceof Object) || (Object.getPrototypeOf(vdom) !== Object.prototype)) return vdom
 
     const {tag, ids, classes, attrs, children} = vdom,
-          id = ids.length ? `ids="${ids.join(' ')}"` : '',
-          _class = classes.length ? `class="${classes.join(' ')}"` : ''
-
+        id = `id="${(ids || []).concat(attrs ? attrs.id : '').join(' ')}"`,
+        _class = `class="${(classes || []).concat(attrs ? attrs.className : '').join(' ')}"`
     const closing = children ? `${children.map(c => html(c)).join(' ')}</${tag}>` : ''
+    // TODO: figure out wtf todo here?
+    // maybe just never use these, only use html() on server rendering?
+    const events = stripEvents(vdom)
+    const _attrs = Object.keys(attrs || POOL.get())
+        .filter(x => reservedAttrs.indexOf(x) === -1)
+        .reduce((a,v,i,arr) => `${a} ${v}="${attrs[v]}"`,'')
 
-    return `<${tag} ${id} ${_class} ${!closing ? '/' : ''}>${closing}`
+    POOL.recycle(vdom)
+
+    return `<${tag} ${id} ${_class} ${_attrs} ${!children ? '/' : ''}>${closing}`
 }
 
 const rAF =
       requestAnimationFrame ||
       webkitRequestAnimationFrame ||
       mozRequestAnimationFrame ||
-      ((cb) => setTimeout(cb, 0))
+      ((cb) => setTimeout(cb, 16.6))
 
 const mounts = new Map()
 
 export const mount = (fn, el) => {
-    render(fn, el)
+    render(fn(), el)
     mounts.set(el, fn)
 }
 
-let _render
-export const render = (fn, el) => {
-    // let b = +new Date
-    _render = html(fn())
-    rAF(() => {
-        el.innerHTML = _render
-    })
-    // log((+new Date - b)+'ms')
-}
-
-export const update = () => {
-    for(let [el,fn] of mounts.entries()){
-        render(fn, el)
+const debounce = (func, wait) => {
+    let timeout = null,
+        calls = 0,
+        memo = (func, ...args) => {
+            timeout = null
+            func(...args)
+        }
+    return (...args) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(memo.bind(null, func, ...args), wait)
     }
 }
 
+const render = debounce((vdom, el) => {
+    rAF(() => simpleRenderingMode ? simpleApply(vdom, el) : applyUpdates(vdom, el))
+}, 16)
+
+export const update = () => {
+    for(let [el,fn] of mounts.entries()){
+        render(fn(), el)
+    }
+}
+
+const createTag = (vdom=POOL.get()) => {
+    if(!(vdom instanceof Object))
+        return document.createTextNode(vdom)
+    let {tag, attrs, ids, classes} = vdom,
+        x = document.createElement(tag),
+        events = stripEvents(vdom)
+
+    applyEvents(events, x)
+    attrs && Object.keys(attrs).forEach(attr => x.setAttribute(attr, attrs[attr]))
+    x.id = ids ? ids.join(' ') : ''
+    x.className = classes ? classes.join(' ') : ''
+    return x
+}
+
+const simpleApply = (vdom, el) =>
+    el.innerHTML = html(vdom)
+
+const applyUpdates = (vdom,el) => {
+    if(!vdom) {
+        el && el.parentElement && el.parentElement.removeChild(el)
+        return
+    }
+
+    let __el = el && el.children,
+        __v = vdom && vdom.children
+
+    if(vdom instanceof Array){
+        __v = vdom
+    } else if(vdom instanceof Object && vdom.tag){
+        if(!el || el.tagName !== vdom.tag) {
+            let t = createTag(vdom)
+            // t.innerHTML = html(vdom.children)
+            el.parentElement.insertBefore(t, el)
+            el.parentElement.removeChild(el)
+            return applyUpdates(__v, t)
+        }
+    } else {
+        let t = document.createTextNode(vdom)
+        el.parentElement.insertBefore(t, el)
+        el.parentElement.removeChild(el)
+        return
+    }
+
+    const len = Math.max(__el.length, __v.length)
+    for(let i=0; i<len; i++){
+        let v = __v[i],
+            d = __el[i]
+
+        if(v && d){
+            applyUpdates(v, d)
+        } else if(v && !d){
+            let t = createTag(v)
+            applyUpdates(v.children, t)
+            el.appendChild(t)
+        } else if (!vdom && d) {
+            d.parentElement.removeChild(d)
+        }
+    }
+
+    // currently clears/zeroes out the data prematurely, need to figure this out
+    // rAF(() => POOL.recycle(vdom))
+}
+
 export const qs = (s='body', el=document) => el.querySelector(s)
+
+/*
+usage:
+
+let component = () =>
+    new Array(20).fill(true).map(x =>
+        m('div', {onMouseOver: e => log(e.target.innerHTML)}, range(1,100)))
+
+client-side
+-----
+mount(component, qs())
+
+server-side (Express)
+-----
+res.send(html(component()))
+
+client-side constant re-rendering
+-----
+const run = () => {
+    setTimeout(run, 20)
+    update()
+}
+run()
+*/
