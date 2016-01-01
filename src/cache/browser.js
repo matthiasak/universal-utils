9e002f562fc12bb3cf2884a70bd2f807320ed45c
Matthew Keas <matthiasak@gmail.com>
2016-01-01 15:04:33 -0600

    removing local forage, buggy bludger

--- src/cache/browser.js
+++ src/cache/browser.js
@@ -1,48 +1,38 @@
-const clone = (obj) =>
-    JSON.parse(JSON.stringify(obj))
-
-const storage = require('localforage')
-
-// Force localStorage to be the backend driver.
-storage.setDriver(storage.LOCALSTORAGE)
+const s = window.localStorage
 
 const cache = () => {
 
     const getItem = (key, expire=false) => {
-        return storage.getItem(key).then(d => {
-            if(!(d && d.data)) throw `${key} not in cache`
-            let expired = expire || (+new Date) > d.expiresAt
-            if(expired) throw `${key} is expired`
-            return d.data
+        const data = s.getItem(key)
+        if(!d) return Promise.reject(`${key} not in cache`)
+        let expired = expire || (+new Date) > d.expiresAt
+        if(expired) return Promise.reject(`${key} is expired`)
+        return Promise.resolve(JSON.parse(d.data))
+    }
+
+    const setItem = (key, data, timeout=5*60*60*1000, expiresAt=(+new Date + timeout)) => {
+        if(!data) return Promise.reject(`data being set on ${key} was null/undefined`)
+        return new Promise((res,rej) => {
+            try{
+                s.setItem(key, JSON.stringify({expiresAt, data}))
+                res(true)
+            }catch (e){
+                rej(`key ${key} has a value of ${val}, which can't be serialized`)
+            }
         })
     }
 
-    const setItem = (key, val, timeout=5*60*60*1000) => {
-        if(!val) return Promise.reject('val was null/undefined')
-        const expiresAt = +new Date + timeout
-        return storage.setItem(key, {expiresAt, data:val})
-    }
-
-    const clearAll = (key) =>
-        storage.keys()
-            .then(keys =>
-                Promise.all(keys.filter(x =>
-                    x.indexOf(key) !== -1).map(x =>
-                        Promise.resolve(x))))
-            .then(keys =>
-                Promise.all(keys.map(k =>
-                    storage.clear(k))))
+    const clearAll = key =>
+        if(!key)
+            s.clear()
+        for(var i in s){
+            if((!key || i.indexOf(key) !== -1) && localstorage.hasOwnProperty(i))
+                s.removeItem(i)
+        }
+        return Promise.resolve(true)
 
     return { getItem, setItem, clearAll }
 }
 
 const c = cache()
 export default c
-
-// ---- extra config options ----
-// window.storage = storage
-// window.cache = c
-// storage.config({
-//     // driver: storage.LOCALSTORAGE,
-//     name: 'rentvillas'
-// })
